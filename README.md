c0357fd16c671b183a73d829e6c994797fc89d59
Kyle E. Mitchell <kyle@kemitchell.com>
2017-10-09 12:55:34 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# CONTRACTS.json JSON Schema
+
+a [JSON Schema](http://json-schema.org/) for `CONTRACTS.json` files
+that describe services contracts available with project maintainers
---
+++ test.js
@@ -0,0 +1,24 @@
+var AJV = require('ajv')
+var assert = require('assert')
+var schema = require('./')
+
+var ajv = new AJV({allErrors: true})
+
+assert(ajv.validateSchema(schema), 'invalid schema')
+
+var validate = ajv.compile(schema)
+
+assert(
+  validate({
+    who: [
+      {
+        name: 'Kyle E. Mitchell',
+        email: 'kyle@kemitchell.com',
+        where: 'California, USA'
+      }
+    ],
+    what: {
+      support: 'https://commonform.org/publications/switchmode/project-summary-open-support/1e1c'
+    }
+  })
+)
